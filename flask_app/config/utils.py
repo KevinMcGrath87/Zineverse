01d32efe868a64eb650d8216ace419afbef7cb82
Kevin McGrath <kpmcgra@gmail.com>
2023-03-30 16:53:06 -0500

    updated file location of upload folder

--- flask_app/config/utils.py
+++ flask_app/config/utils.py
@@ -1,6 +1,5 @@
 import os
 from flask_app.__init__ import app
-
-app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(os.getcwd()),'solo_project','flask_app','static','zinelib')
+app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(os.getcwd()),'cd_final_project','flask_app','static','zinelib')
 UPLOAD_FOLDER = app.config['UPLOAD_FOLDER']
 ALLOWED_EXTENSIONS = {'pdf','png','jpg','jpeg','svg'}
